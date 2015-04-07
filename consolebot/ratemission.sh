b2e6eaa275a2ea48a01bdad77f2565e8ad5753bf
Roman Ananyev <me@rananyev.ru>
2015-04-07 19:58:12 +0300

    WAT

--- consolebot/ratemission.sh
+++ consolebot/ratemission.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-MISSIONGUID=$1
-RATING=$2
-LAT=`cat cur.txt|cut -d, -f1`
-LON=`cat cur.txt|cut -d, -f2`
-echo starting mission $MISSIONGUID with rating $RATING
-bin/startMission.sh $LAT $LON $MISSIONGUID
-sleep 1
-bin/abortMission.sh $LAT $LON $MISSIONGUID
-sleep 1
-bin/rateMission.sh $LAT $LON $MISSIONGUID $RATING
+. bin/func.inc
+LAT=$1
+LON=$2
+MISSIONGUID=$3
+RATING=$4
+DELTALAT=$(($RANDOM%40-20))
+DELTALON=$(($RANDOM%40-20))
+LATE6=`echo $DELTALAT+$LAT*1000000/1|bc`
+LONE6=`echo $DELTALON+$LON*1000000/1|bc`
+HEXLATE6=`HEXE6   $LATE6`
+HEXLONE6=`HEXE6   $LONE6`
+#check input for regmatch
+./bin/rateMission.pl $HEXLATE6 $HEXLONE6 $MISSIONGUID $RATING |gzip  > payload.gz
+./bin/postapi.sh rateMission
