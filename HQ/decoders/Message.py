73e42e7d81e6b8a639a5da75789f0cae5c79cacb
neronmoon <alistar.neron@gmail.com>
2015-09-26 13:03:27 +0200

    decoders proto

---
+++ HQ/decoders/Message.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+
+class Message(object):
+    def parse(self, dict):
+        seconds, millis = divmod(dict[1], 1000)
+        time = datetime.fromtimestamp(seconds) + timedelta(milliseconds=millis)
+        return {
+            'guid': dict[0],
+            'time': time.strftime('%Y/%m/%d %H:%M:%S:%f')[:-3],
+            'text': dict[2]['plext']['text'],
+            'type': dict[2]['plext']['plextType'],
+            'team': dict[2]['plext']['team']
+        }
